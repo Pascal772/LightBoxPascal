35f0fb7f3f584b8f942e2fe3e034a0cae9a4ec88
Pascal772 <pascal772@gmail.com>
2021-11-16 14:46:39 +0100

    MAJ

--- script.js
+++ script.js
@@ -1,5 +1,11 @@
 let image = document.querySelector("a")
-image.addEventListener("click", function() {
+
+let modal = document.querySelector('#modale');
+
+// Fermeture de modale
+let span = document.querySelector(".close")[0];
+
+// image.addEventListener("click", function() {
 
     
-});
+// });
